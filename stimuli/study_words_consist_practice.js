0c5e3158dab2806a95da808a16e966f08f9b87e0
Jackie Spear <jackiespear44@gmail.com>
2023-01-13 14:41:03 -0600

    Adding Experimental files

---
+++ stimuli/study_words_consist_practice.js
@@ -0,0 +1,4 @@
+studywords_consist_practice = [
+{"stimulus":"<p style='font-size: 48px;'>RADIO</p>","data":{"type":"practice","category":"consistent","word":"RADIO"}},
+{"stimulus":"<p style='font-size: 48px;'>PENCIL</p>","data":{"type":"practice","category":"consistent","word":"PENCIL"}},
+]
