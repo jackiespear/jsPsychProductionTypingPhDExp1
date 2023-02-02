c18093606bf7b80f5103db68c532a0600681573c
jackiespear <jackiespear44@gmail.com>
2023-02-02 16:31:07 -0600

    Update study_words_consist_practice.js

--- stimuli/study_words_consist_practice.js
+++ stimuli/study_words_consist_practice.js
@@ -1,5 +1,5 @@
 studywords_practice = [
-{"stimulus":"<p style='font-size: 48px;'>BRICK - <span style='color: red'>TRICK</span></p>","data":{"type":"practice","category":"consistent","word":"RADIO1"}},
-{"stimulus":"<p style='font-size: 48px;'>WRITE - <span style='color: green'>FRIGHT</span></p>","data":{"type":"practice","category":"consistent","word":"PENCIL2"}},
+{"stimulus":"<p style='font-size: 48px;'>BRICK - <span style='color: red'>TRICK</span></p>","data":{"type":"practice","category":"read","word":"TRICK"}},
+{"stimulus":"<p style='font-size: 48px;'>WRITE - <span style='color: green'>FRIGHT</span></p>","data":{"type":"practice","category":"produce","word":"FRIGHT"}},
 ]
 
